c994e71036e62f44b86daf204fc90af3dd1bd9e4
Ben <bdhsu@berkeley.edu>
2019-01-03 23:12:00 -0800

    SelectLanguage functional component spun out of Popular

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,9 +1,58 @@
 var React = require('react');
+var PropTypes = require('prop-types');
+
+class SelectLanguage extends React.Component {
+    render () {
+        const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+        return (
+            <ul className='languages'>
+                {/*<p>Selected Language: {this.props.selectedLanguage}</p>*/}
+                {languages.map(lang => {
+                    return (
+                        <li
+                            style={lang === this.props.selectedLanguage ? {color: '#d0021b'}: null}
+                            onClick={this.props.onSelect.bind(null, lang)}
+                            key={lang}>
+                            {lang}
+                        </li>
+                    )
+                })}
+            </ul>
+        )
+    }
+}
+
+SelectLanguage.propTypes = {
+    selectedLanguage: PropTypes.string.isRequired,
+    onSelect: PropTypes.func.isRequired,
+}
 
 class Popular extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedLanguage: 'All'
+        };
+
+        this.updateLanguage = this.updateLanguage.bind(this);
+    }
+    updateLanguage(lang) {
+        this.setState(_ => {
+            return {
+                selectedLanguage: lang
+            }
+        });
+    }
     render() {
         return (
-            <div>Popular</div>
+            <div>
+                <SelectLanguage
+                    selectedLanguage={this.state.selectedLanguage}
+                    onSelect={this.updateLanguage}
+                />
+            </div>
+
         )
     }
 }
